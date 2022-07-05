e5466f3d4e67beae61491376cef93c7f8f95ad45
YusunPark <solarpy@hanyang.ac.kr>
2022-07-05 21:08:16 +0900

    Add styling of index.tsx

--- nextjs-intro/pages/index.tsx
+++ nextjs-intro/pages/index.tsx
@@ -1,3 +1,5 @@
+/// <reference types="styled-jsx" />
+
 import { useEffect, useState } from "react";
 const API_KEY = "27b04d28b3c48267ca2d4119b0197e69";
 
@@ -16,14 +18,37 @@ export default function Home() {
     })();
   }, []);
   return (
-    <div>
+    <div className="container">
+      {" "}
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie:any) => (
-        <div key={movie.id}>
+      {movies?.map((movie: any) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
